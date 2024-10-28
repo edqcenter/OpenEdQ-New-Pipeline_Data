e75e4f1ba24b10fb6e5d4b6125f864871df23173
Jimmy Luong <jimmyvluong@gmail.com>
2024-10-28 16:05:53 -0700

    update readme and add sample queries

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Open scripts for working with ERS data.
 
 - Most scripts are written as one common table expression (CTE) to perform a single, logical unit of work.
+- The file structure and data types of the final script, 5, follow the the structure as the ERS files received from
+the Institutional Research and Analyses team at the CO.
---
+++ open_scripts/x_sample_queries.sql
@@ -0,0 +1,20 @@
+/*
+Sample queries
+*/
+
+select 
+	count(*) as total_count
+	, cred_obj_description
+from [core_ers].[vw_core_enrollment_count_and_completion_rate]
+where erss_year = 2019
+group by cred_obj_description
+order by total_count DESC;
+
+select 
+	Credential_Type
+	, count(*) as total_count
+from [core_ers].[vw_core_enrollment_count_and_completion_rate]
+where erss_year = 2021
+group by 1
+order by total_count DESC;
+
