7295b16e63b1a6962b665cf9f619f14814112368
Jimmy Luong <jimmyvluong@gmail.com>
2024-10-29 15:44:36 -0700

    update International Student logic to align with IR & A

--- open_scripts/3_create_vw_international_students_addition.sql
+++ open_scripts/3_create_vw_international_students_addition.sql
@@ -13,12 +13,25 @@ select
   *,
   /*
   This case statement re-labels Race/Ethnicity to align with IR & A dashboards, which include the category "International Student".
-  One more step needs to happen to replicate the numbers: pre-filtering based on logic in the IPEDS website.
+
+  F: Non-US citizen, F visa (student)
+  J: Non-US citizen, J visa (exchange visitor)
+  O: Non-US citizen, other visa
+  N: Non-US citizen, undetermined status or no visa required because not entering the US
+
+  Notes: As of Fall 2022 according to the Student Success Dashboards, students whose citizenship code value included Non-US citizen, 
+  undetermined status or no Visa require are no longer categorized as part of the International group.
+  This means erss_cit 'O' and 'F" are still considered "International Student" for Fall 2022 onward.
   */
   case
   ---- Non-U.S. citizen
   when
-   erss_cit IN ('F','O','N')
+   erss_cit IN ('F','J','O','N')
+   and erss_year IN (2019,2020,2021)
+  then 'International Student'
+  when
+   erss_cit IN ('F','O')
+   and erss_year IN (2022,2023)
   then 'International Student'
     else race_description
     end as race_desc_with_intl
--- open_scripts/x_sample_queries.sql
+++ open_scripts/x_sample_queries.sql
@@ -6,7 +6,7 @@ select
 	count(*) as total_count
 	, cred_obj_description
 from [core_ers].[vw_core_enrollment_count_and_completion_rate]
-where erss_year = 2019
+where erss_year = 2021
 group by cred_obj_description
 order by total_count DESC;
 
@@ -14,7 +14,7 @@ select
 	Credential_Type
 	, count(*) as total_count
 from [core_ers].[vw_core_enrollment_count_and_completion_rate]
-where erss_year = 2021
+where erss_year = 2022
 group by Credential_Type
 order by total_count DESC;
 
